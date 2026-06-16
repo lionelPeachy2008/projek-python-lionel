9ee9c9aa78f3956560143afb89258f8a19b9b493
Lionel <lionelevelyn2008@gmail.com>
2026-06-16 21:08:35 +0700

    ditambah perkalian

--- projek_github.py
+++ projek_github.py
@@ -1,2 +1,6 @@
 print("halo ini projek githubku")
 print("Kodingan ini diperbarui dari laptop kedua Lionel yaa")
+angka = int(input("Masukan angka terserah : "))
+print("Angka anda: ",angka)
+hasil = angka * 10
+print("Hasil: ",hasil)
