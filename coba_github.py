027b16fa64a318b8cab576b0915a6ee9ad0de6c4
Lionel <lionelevelyn2008@gmail.com>
2026-06-19 12:28:47 +0700

    ubah nama file

--- coba_github.py
+++ coba_github.py
@@ -1,6 +1,7 @@
 print("halo ini projek githubku")
 print("Kodingan ini diperbarui dari laptop kedua Lionel yaa")
 angka = int(input("Masukan angka terserah : "))
-print("Angka anda: ",angka)
+print("Projek ini ganti nama ya")
+print("Angka anda:", angka)
 hasil = angka * 10
 print("Hasil: ",hasil)
